6401f93a5bfa7d04f7b8b111d159b53f1c941889
MALWICK <batahumphrey66@gamil.com>
2023-06-27 16:42:46 +0100

    created footer

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -3,6 +3,7 @@ import HeroGallery from './HeroGallery/HeroGallery';
 import Sticky from '../../component/stickybar/Stickybar';
 import Detail from '../../component/detailnavbar/Detailnavbar';
 import Gallerys from '../../component/gallerys/Gallerys';
+import Footer from '../../component/Footer/Footer';
 import Styles from './Gallery.module.css';
 
 function Gallery() {
@@ -12,6 +13,7 @@ function Gallery() {
       <Detail />
       <HeroGallery />
       <Gallerys />
+      <Footer />
     </div>
   );
 }
